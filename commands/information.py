07d2b5f43de7145afb5ffbc27083d8961498d2c1
Clonexy <conexy700@gmail.com>
2022-07-26 20:42:39 +0700

    fsd

--- commands/information.py
+++ commands/information.py
@@ -8,6 +8,14 @@ from config import settings
 from easy_pil import *
 
 
+def format_seconds_to_hhmmss(seconds):
+    hours = seconds // (60 * 60)
+    seconds %= (60 * 60)
+    minutes = seconds // 60
+    seconds %= 60
+    return "%02i:%02i:%02i" % (hours, minutes, seconds)
+
+
 class InformationSender(commands.Cog):
     def __init__(self, client):
         self.client = client
@@ -33,18 +41,22 @@ class InformationSender(commands.Cog):
                 roles = ' '.join([r.mention for r in user.roles[1:15]])
             # Свистопляски с целью вставки аватарки в Background.png, которая накладывается туда по типу маски и уже
             # готовое изображение постится самим ботом
-
-            background = Editor('./assets/profile_backgrounds/background.png')
+            db = sqlite3.connect("./databases/main.sqlite")
+            cursor = db.cursor()
+            warn_count = cursor.execute(f"SELECT warns FROM warns WHERE user_id = {user.id}").fetchone()[0]
+            if warn_count == 0:
+                background = Editor('./assets/profile_backgrounds/bg.png')
+            elif warn_count == 1:
+                background = Editor('./assets/profile_backgrounds/bg_2.png')
+            else:
+                background = Editor('./assets/profile_backgrounds/bg_3.png')
             avatar = BytesIO()
             await user.display_avatar.with_format("png").save(avatar)
             profile_picture = Image.open(avatar)
-            profile = Editor(profile_picture).resize((550, 550)).rounded_corners(radius=50)
+            profile = Editor(profile_picture).resize((360, 360)).rounded_corners(radius=50)
             larger_font = Font('ARIALUNI.TTF', 65)
             font = Font('ARIALUNI.TTF', 50)
-            background.paste(profile, (250, 300))
-            db = sqlite3.connect("./databases/main.sqlite")
-            cursor = db.cursor()
-
+            background.paste(profile, (230, 360))
             cursor.execute(f"SELECT lvl, exp FROM levels WHERE user_id = {user.id}")
             leveling = cursor.fetchone()
             level = leveling[0]
@@ -67,15 +79,26 @@ class InformationSender(commands.Cog):
                 balance = balance[0]
             except:
                 return await ctx.send('что-то с бд!!!')
-            warn_count = cursor.execute(f"SELECT warns FROM warns WHERE user_id = {user.id}").fetchone()[0]
+            cursor.execute(f"SELECT pair_id FROM marriage WHERE user_id = {ctx.author.id}")
+            partner = cursor.fetchone()
+            try:
+                partner = partner[0]
+            except:
+                return await ctx.send('что-то с бд!!!')
+            if partner == 0:
+                partner_text = '-'
+            else:
+                partner_text = await self.client.fetch_user(partner)
+                partner_text = partner_text.name
             cursor.close()
             db.close()
-            background.text((1250, 310), str(user), font=larger_font, color="#FFFFFF")
-            background.text((1250, 425), f"УРОВЕНЬ: {cur_lvl}, ОПЫТ: {cur_xp}/{next_xp}", font=font, color="#FFFFFF")
-            background.text((1250, 525), f"ВРЕМЯ В ВОЙСАХ: {voice_time}", font=font, color="#FFFFFF")
-            background.text((1250, 630), f"БАЛАНС: {balance}", font=font, color="#FFFFFF")
-            background.text((1250, 735), f"{user.joined_at.strftime(date_format)}", font=font, color="#FFFFFF")
-            background.text((1250, 835), f"ВАРНЫ: {warn_count}", font=font, color="#FFFFFF")
+            background.text((550, 765), str(user.name), font=font, color="#FFFFFF")
+            background.text((450, 865), str(partner_text), font=font, color="#FFFFFF")
+            background.text((1200, 935), f"{cur_lvl} LVL", font=font, color="#FFFFFF")
+            background.text((1200, 835), f"{format_seconds_to_hhmmss(float(voice_time))}", font=font, color="#FFFFFF")
+            background.text((1200, 735), f"{balance}", font=font, color="#FFFFFF")
+            background.text((1000, 465), f"{user.joined_at.strftime(date_format)}", font=font, color="#FFFFFF")
+            background.text((1000, 585), f"{user.created_at.strftime(date_format)}", font=font, color="#FFFFFF")
             embed.add_field(name=f'{star} На сервер присоединился:',
                             value=f'```\n{user.joined_at.strftime(date_format)}\n```',
                             inline=False)
@@ -85,7 +108,8 @@ class InformationSender(commands.Cog):
             role = self.client.get_emoji(996245790357999646)
             embed.add_field(name=f"{level} Уровень", value=f'```\n{cur_lvl}\n{cur_xp}|{next_xp}\n```', inline=True)
             embed.add_field(name=f'{credit_card} Баланс:', value=f'```{balance}```', inline=True)
-            embed.add_field(name=f"{voice} Время в войсах", value=f"```{voice_time}```", inline=True)
+            embed.add_field(name=f"{voice} Время в войсах",
+                            value=f"```{format_seconds_to_hhmmss(float(voice_time))}```", inline=True)
             embed.add_field(name=f"{role} Роли", value=roles, inline=True)
             file = nextcord.File(fp=background.image_bytes, filename="bv.png")
             embed.set_image(url="attachment://bv.png")
